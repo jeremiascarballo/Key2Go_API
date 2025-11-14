aa22e1a75c499fbd00ae54dced9cee08ee8f7fab
jeremiascarballo <JeremiasCarballo03@gmail.com>
2025-11-14 13:59:21 -0300

    Fix: delete non-null atributtes in TripResponse

--- Contract/Trip/Response/TripResponse.cs
+++ Contract/Trip/Response/TripResponse.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public int ReservationNumber { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public int? InitialKm { get; set; }
         public int? FinalKm { get; set; }
         public int Status { get; set; }
